f676d72dee18ffbaef162f80f1938cc1722ef450
Matt Janda <matt.janda.kingston@gmail.com>
2023-09-24 17:39:45 +0100

    Add string parsers (#7)
    * Add c string parsers to byte slide
    
    * Add pascal string parser to byte slide

--- src/AutoByte.Tests/ByteSlide_Must.cs
+++ src/AutoByte.Tests/ByteSlide_Must.cs
@@ -1,4 +1,5 @@
 ﻿using System.Buffers.Binary;
+using System.Text;
 
 namespace AutoByte.Tests
 {
@@ -83,5 +84,64 @@ namespace AutoByte.Tests
             Assert.Equal(slide.GetCDateUtcLittleEndian(), EXPECTED_DATE_TIME);
         }
 
+
+
+
+        [Fact]
+        public void GetCString_ReturnsExpectedStirngAndCompleteSlide()
+        {
+            byte[] TWO_C_STRINGS = {
+                0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x00, 0x4D, 0x61, 0x74, 0x74,
+                0x20, 0x4A, 0x61, 0x6E, 0x64, 0x61, 0x00
+            };
+
+            string EXPECTED_FIRST_TEXT = "Hello World";
+            string EXPECTED_SECOND_TEXT = "Matt Janda";
+
+            var slide = new ByteSlide(TWO_C_STRINGS);
+
+            Assert.Equal(slide.GetCString(Encoding.ASCII), EXPECTED_FIRST_TEXT);
+            Assert.Equal(slide.GetCString(Encoding.ASCII), EXPECTED_SECOND_TEXT);
+
+            Assert.Equal(0, slide.Length);
+        }
+
+
+        [Fact]        
+        public void GetCString_ThrowsUnterminatedCStringException()
+        {            
+            byte[] UNTERMINATED_STRING = {
+                0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64
+            };
+
+            
+            Assert.Throws<ByteSlideException>(() => {
+                var slide = new ByteSlide(UNTERMINATED_STRING);
+                slide.GetCString(Encoding.ASCII); 
+            });
+        }
+
+
+        [Fact]
+        public void GetPascalString_ReturnsExcpectedStrings()
+        {
+            byte[] PASCAL_STRINGS = {
+                0x0B, 0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x57, 0x6F, 0x72, 0x6C, 0x64, 0x0A, 0x4D, 0x61, 0x74,
+                0x74, 0x20, 0x4A, 0x61, 0x6E, 0x64, 0x61, 0x00
+            };
+
+            var slide = new ByteSlide(PASCAL_STRINGS);
+            
+            Assert.Equal("Hello World", slide.GetPascalString(Encoding.ASCII));
+            Assert.Equal("Matt Janda", slide.GetPascalString(Encoding.ASCII));
+            Assert.Equal("", slide.GetPascalString(Encoding.ASCII));
+
+            Assert.Equal(0, slide.Length);
+
+        }
+
+
+
+       
     }
 }
